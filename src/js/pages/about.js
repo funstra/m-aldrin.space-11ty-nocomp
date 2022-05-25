117021fbe3a232967e10bed6d2819cdf6a4318df
funstra <founstra@gmail.com>
2022-05-25 23:04:51 +0200

    better type and more animation

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,28 +1,26 @@
 if (!window._initPath) {
   window._initPath = () => {
-    setTimeout(() => {
-      const doc = document.querySelector('main[router\\:page="/about/"]');
-      const hello = doc.querySelector(".hello").parentElement;
-      const pathIllu = doc.querySelector(".about .path-illu");
-      const faceIllu = doc.querySelector(".about .face-illu");
-      const path = pathIllu.querySelector("path");
-      const endPoint = faceIllu.querySelector("circle");
-      const getCoord = e => {
-        const endPointRect = endPoint.getBBox();
-        path.setAttribute(
-          "d",
-          `
+    const doc = document.querySelector('main[router\\:page="/about/"]');
+    const hello = doc.querySelector(".hello").parentElement;
+    const pathIllu = doc.querySelector(".about .path-illu");
+    const faceIllu = doc.querySelector(".about .face-illu");
+    const path = pathIllu.querySelector("path");
+    const endPoint = faceIllu.querySelector("circle");
+    const getCoord = e => {
+      const endPointRect = endPoint.getBBox();
+      path.setAttribute(
+        "d",
+        `
           M${hello.offsetLeft + 20}, ${hello.offsetTop} 
           m0,-8
           m-3,-4
           Q${endPointRect.x * 0.75} ${endPointRect.y * 1} ${endPointRect.x} ${
-            endPointRect.y
-          }
+          endPointRect.y
+        }
     `
-        );
-      };
-      getCoord();
-      window.addEventListener("resize", getCoord);
-    }, 5);
+      );
+    };
+    getCoord();
+    window.addEventListener("resize", getCoord);
   };
 }
